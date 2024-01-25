e34b9d9418ce7262f168f69ff4a445e8e95f7521
bektidev <bekti@primaku.com>
2024-01-26 03:07:30 +0700

    chore: add authentication, authorization, refactor code

---
+++ authentication/authorizations.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+from fastapi import Depends
+from jose import JWTError, jwt
+from config.envloaders import JWT_SECRET, JWT_ALGORITHM
+
+from fastapi.security import OAuth2PasswordBearer
+from utils.exceptions import CustomException
+from const.error_types import Error
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="")
+
+def jwt_required(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return token
+    except JWTError:
+        raise CustomException(status=401, message=Error.UNAUTHORIZED)
---
+++ const/error_types.py
@@ -0,0 +1,7 @@
+class Error:
+    UNAUTHORIZED='Unauthorized'
+    INVALID_CREDENTIAL='Invalid Credential'
+    NOTFOUND='Not found'
+
+class Ok:
+    HEALTH_CHECK='It was nice so far :D'
